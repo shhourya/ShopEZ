edf0cb82b9d4743cc47b600bdf0b4c1f91c8d74d
shhourya <shhourya14459@gmail.com>
2026-06-17 16:48:15 +0530

    Add 'admin' role to User model

--- server/models/User.js
+++ server/models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['customer', 'seller'],
+      enum: ['customer', 'seller', 'admin'],
       default: 'customer',
     },
   },
