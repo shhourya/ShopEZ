9841828436ab08d8d09aa9ba3d518a1c3dd289bb
shhourya <shhourya14459@gmail.com>
2026-06-17 15:46:04 +0530

    Change user role enum to include 'seller'

--- server/models/User.js
+++ server/models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['customer', 'admin'],
+      enum: ['customer', 'seller'],
       default: 'customer',
     },
   },
