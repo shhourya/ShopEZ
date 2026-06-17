e1a207d0baa4dc0eb15e4dc43fbd784036dbee19
shhourya <shhourya14459@gmail.com>
2026-06-17 15:40:52 +0530

    Change user role enum to 'admin'

--- server/models/User.js
+++ server/models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['customer', 'seller'],
+      enum: ['customer', 'admin'],
       default: 'customer',
     },
   },
